d9d0da0b9aea963d50b1c3ed6fe0c2740db79500
okumu87 <okumu793@gmail.com>
2025-02-21 10:52:19 +0100

    Smooth scrolling implemented on section-1

--- script.js
+++ script.js
@@ -50,3 +50,30 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+message.style.backgroundColor = 'lightgrey';
+
+// smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // position on the page section
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
