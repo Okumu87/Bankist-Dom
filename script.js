787edce3bfe569ac4aa2a6d5fb99e8ebd5a4c9a1
okumu87 <okumu793@gmail.com>
2025-02-24 12:25:37 +0100

    Adding scroll using intersection observer

--- script.js
+++ script.js
@@ -139,3 +139,42 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 // mouse out
 nav.addEventListener('mouseover', handleHover.bind(1));
+
+// sticky nav
+
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+// window.addEventListener('scroll', function () {
+//   console.log(this.window.scrollY);
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove(sticky);
+};
+const headerObserver = new IntersectionObserver();
+stickyNav,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-90px',
+  };
+
+headerObserver.observe(header);
